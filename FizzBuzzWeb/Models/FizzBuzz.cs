b02bac2d421a60a3ae55b8e78103680b39a287f4
Szymon Fiedosiuk <szymonfiedosiuk@wp.pl>
2022-03-19 13:57:09 +0100

    New Custom validator

---
+++ FizzBuzzWeb/Models/FizzBuzz.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FizzBuzzWeb.Models
+{
+    public class FizzBuzz
+    {
+        [Required, Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}."), CustomValidator()]
+
+        [Display(Name = "Twoj szczesliwy numerek")]
+        public int? Number { get; set; }
+
+    }
+}
--- FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FizzBuzzWeb.Models;
 
 namespace FizzBuzzWeb.Pages
 {
     public class IndexModel : PageModel
     {
+        public CustomValidator CustomValidator { get; set; }
         private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet=true)]
+        public string name {get; set; }
+
+        [BindProperty]
+        public FizzBuzz FizzBuzz { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +21,21 @@ namespace FizzBuzzWeb.Pages
 
         public void OnGet()
         {
-
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "User";
+            }
         }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            
+            return RedirectToPage("./Privacy");
+        }
+
     }
 }
